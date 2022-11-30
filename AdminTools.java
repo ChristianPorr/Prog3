de323f7d3200fdbd3aa7ac65088a74f603f82ece
SofianAgouzoul <88892858+SofianAgouzoul@users.noreply.github.com>
2022-11-30 11:22:43 +0100

    Delete AdminTools.java

--- AdminTools.java
+++
@@ -1,5 +0,0 @@
-import java.util.*;
-public abstract class AdminTools{
-   public abstract void aggiungiPrenotazione(ArrayList<Tavolo> lista, int n, String nominativo, double h);
-   public abstract void eliminaPrenotazione(Tavolo numT);
-   }
