3aa96ca1feb97e946bef0b84bf87a1484123ab79
SofianAgouzoul <88892858+SofianAgouzoul@users.noreply.github.com>
2022-11-30 11:24:24 +0100

    Delete Cucina.java

--- Cucina.java
+++
@@ -1,7 +0,0 @@
-
-public interface Cucina{
-    public abstract void ordinePresoInCarico(Tavolo obj);
-    public abstract void ordineInLavorazione(Tavolo obj);
-    public abstract void ordineCompletato(Tavolo obj);
-
-}
