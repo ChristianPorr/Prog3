d108d70902f66685bda2657793229ec5ca2ddff2
SofianAgouzoul <88892858+SofianAgouzoul@users.noreply.github.com>
2023-01-16 18:46:31 +0100

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -8,23 +8,23 @@ import java.awt.event.ActionEvent;
 
 public abstract class Frame {
     //istruzioni standard
+	JPanel panel = new JPanel(new BorderLayout());
     JButton btnCassa = new JButton();
-    JButton btnCameriere = new JButton();
     JButton btnCliente = new JButton();
     ImageIcon img = new ImageIcon("icon.png");
     JFrame frame = new JFrame();
     JLabel label = new JLabel();
     JMenuBar menuBar = new JMenuBar();
-
+    
+    
     public Frame(){
+    
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(900,600);
+    frame.setSize(800,600);
     frame.setIconImage(img.getImage());
-    //frame.setLayout(new FlowLayout(FlowLayout.LEFT));//LEADING E' TUTTO A SINISTRA, senza e' centrato
-    //frame.setLayout(new FlowLayout());
     frame.setResizable(false);
     frame.setVisible(true);
     frame.setLocationRelativeTo(null);
-
+    
     }
 }
--- MainFrame.java
+++ MainFrame.java
@@ -9,34 +9,85 @@ import java.awt.event.ActionEvent;
 
 
 public class MainFrame extends Frame{
-    JButton btnCassa, btnCameriere, btnCliente; 
-    
-    MainFrame(){
+    JButton btnCassa, btnCliente; 
+    JPanel panel,pannelloClienti,pannelloCassa;
 
-    	
+	MainFrame(){	
+		JLabel label = new JLabel("Benvenuti");
+		panel = new JPanel(new BorderLayout());
+		pannelloClienti = new JPanel(new GridBagLayout());
+		pannelloCassa = new JPanel(new GridBagLayout());
         btnCassa = new JButton();
         btnCliente = new JButton();
-
+        
+        panel.add(pannelloClienti,BorderLayout.PAGE_START);
+        panel.add(pannelloCassa,BorderLayout.CENTER);
+        
+        Border bordoClienteInterno = BorderFactory.createTitledBorder("Area Clienti");
+		Border bordoClienteEsterno = BorderFactory.createEmptyBorder(10,10,10,10);
+		Border bordoClienteFinale = BorderFactory.createCompoundBorder(bordoClienteInterno, bordoClienteEsterno);
+		pannelloClienti.setBorder(bordoClienteFinale);
+		
+		Border bordoCassaInterno = BorderFactory.createTitledBorder("Area Cassa");
+		Border bordoCassaEsterno = BorderFactory.createEmptyBorder(10,10,10,10);
+		Border bordoCassaFinale = BorderFactory.createCompoundBorder(bordoCassaInterno, bordoCassaEsterno);
+		pannelloCassa.setBorder(bordoCassaFinale);
+		
+		
+		
+	
+        frame.setTitle("Gestionale Ristorante");
+		frame.setSize(850,600);
+		frame.setIconImage(img.getImage());
+        frame.setResizable(false);
+        frame.setVisible(true); 
+        frame.add(panel);
+        
+        panel.setVisible(true);
+        
         btnCassa.setText("Cassa");
-        btnCliente.setText("Cliente");
         btnCassa.addActionListener(e -> {frame.dispose(); new CassaFrame();});
         btnCassa.setFocusable(false);
+        
+        btnCliente.setText("Cliente");
         btnCliente.setFocusable(false);
         btnCliente.addActionListener(e -> {frame.dispose(); new ClienteFrame();});
         
-		frame.setTitle("Gestionale Ristorante");
-		frame.setSize(900,600);
-		frame.setIconImage(img.getImage());
-		//frame.add(btnCameriere);
-        frame.add(btnCassa);
-        frame.add(btnCliente);
-        frame.setLayout(new FlowLayout(FlowLayout.CENTER));//LEADING E' TUTTO A SINISTRA, senza e' centrato
-        frame.setResizable(false);
-        frame.setVisible(true);
+        ///GBC
+        GridBagConstraints gbc = new GridBagConstraints();
+        
+        //Bottone Cliente
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        
+        gbc.weightx = 0.01;
+        gbc.weighty = 0.01;
+        
+        gbc.gridwidth = 3;
+        
+        gbc.ipadx = 30;
+        gbc.ipady = 30;
+        
+        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        
+        pannelloClienti.add(btnCliente,gbc);
+        
+        //Bottone Cassa
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        
+        gbc.weightx = 0.01;
+        gbc.weighty = 0.01;
+        
+        gbc.gridwidth = 3;
+        
+        gbc.ipadx = 30;
+        gbc.ipady = 30;
+        
+        gbc.anchor = GridBagConstraints.LINE_START;
+        pannelloCassa.add(btnCassa,gbc);
         
         
-        JTextArea textArea = new JTextArea(30,30);
-           
     }
 
 }
